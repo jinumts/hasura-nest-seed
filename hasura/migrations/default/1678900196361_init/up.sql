5f2038cb45d33d3bb919cd1c98c45f2fb56abe3e
JINUMT\91949 <jinu.thomas@perfomatix.com>
2023-03-15 22:48:58 +0530

    initial commit -hasura

--- hasura/migrations/default/1678900196361_init/up.sql
+++ hasura/migrations/default/1678900196361_init/up.sql
@@ -2,6 +2,6 @@ SET check_function_bodies = false;
 CREATE TABLE public.test (
     id uuid NOT NULL
 );
-COMMENT ON TABLE public.test IS 'test';
+COMMENT ON TABLE public.test IS 'test table';
 ALTER TABLE ONLY public.test
     ADD CONSTRAINT test_pkey PRIMARY KEY (id);
